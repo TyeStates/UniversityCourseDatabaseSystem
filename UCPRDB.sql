3bd093539921e37e4a525571ee5690f709514d6d
hisnbrg <150373331+hisnbrg@users.noreply.github.com>
2025-03-28 12:59:44 -0300

    Update UCPRDB.sql

--- UCPRDB.sql
+++ UCPRDB.sql
@@ -7,19 +7,21 @@ DROP TABLE IF EXISTS student;
 DROP TABLE IF EXISTS prerequisites;
 DROP TABLE IF EXISTS courseSection;
 DROP TABLE IF EXISTS course;
-DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS departmentAdmin;
 DROP TABLE IF EXISTS professor;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS user;
 
-CREATE TABLE student (
-    std_name varchar(127) NOT NULL,
-    std_id BIGINT PRIMARY KEY,
-    std_major varchar(127), -- csv string
-    std_yearofstudy int NOT NULL,
-    std_email varchar(127) NOT NULL
+CREATE TABLE user (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(100) NOT NULL,
+    user_pass VARCHAR(255) NOT NULL,
+    user_role ENUM('student', 'professor', 'd_admin', 'admin') NOT NULL
 );
 
 CREATE TABLE department (
-    d_name varchar(255) PRIMARY KEY,
+    d_id INT AUTO_INCREMENT PRIMARY KEY,
+    d_name varchar(255),
     d_code varchar(4) NOT NULL,
     d_email varchar(255),
     d_phone BIGINT,
@@ -27,13 +29,30 @@ CREATE TABLE department (
     d_school varchar(127) NOT NULL
 );
 
+CREATE TABLE departmentAdmin (
+    da_id INT PRIMARY KEY,
+    d_id INT NOT NULL UNIQUE,
+    FOREIGN KEY (da_id) REFERENCES user(user_id),
+    FOREIGN KEY (d_id) REFERENCES department(d_id)
+);
+
+CREATE TABLE student (
+    std_id INT PRIMARY KEY,
+    std_name varchar(127) NOT NULL,
+    std_number BIGINT UNIQUE, 
+    std_major varchar(127), -- csv string
+    std_yearofstudy int NOT NULL,
+    std_email varchar(127) NOT NULL,
+    FOREIGN KEY (std_id) REFERENCES user(user_id)
+);
+
 CREATE TABLE course (
     c_name varchar(127) NOT NULL, 
     c_id varchar(15) PRIMARY KEY,
-    c_department varchar(255),
+    c_department INT,
     c_sharedwith VARCHAR(63), -- csv string 
     c_connectedlab VARCHAR(15),
-    FOREIGN KEY (c_department) REFERENCES department(d_name)
+    FOREIGN KEY (c_department) REFERENCES department(d_id)
 );
 
 CREATE TABLE prerequisites (
@@ -45,14 +64,17 @@ CREATE TABLE prerequisites (
 );
 
 CREATE TABLE professor (
-    p_name VARCHAR(127) PRIMARY KEY, 
+    p_id INT PRIMARY KEY,
+    p_name VARCHAR(127), 
     p_email varchar(127) NOT NULL,
-    p_department VARCHAR(127), -- csv string
-    p_office varchar(127)
+    p_department INT,
+    p_office varchar(127),
+    FOREIGN KEY (p_id) REFERENCES user(user_id),
+    FOREIGN KEY (p_department) REFERENCES department(d_id)
 );
 
 CREATE TABLE coursesTaken (
-    std_id BIGINT,
+    std_id INT,
     c_id varchar(15),
     PRIMARY KEY (std_id, c_id),
     FOREIGN KEY (std_id) REFERENCES student(std_id),
@@ -60,7 +82,7 @@ CREATE TABLE coursesTaken (
 );
 
 CREATE TABLE enrolled (
-    std_id BIGINT,
+    std_id INT,
     c_id varchar(15),
     PRIMARY KEY (std_id, c_id),
     FOREIGN KEY (std_id) REFERENCES student(std_id),
@@ -72,9 +94,9 @@ CREATE TABLE courseSection (
     s_course VARCHAR(15) NOT NULL,
     s_loc varchar(127),
     s_professor varchar (127),
-    s_days JSON, -- might change to csv string
-    s_start TIME, -- enter as '00:00' format on 24hr clock
-    s_end TIME, 
+    s_days varchar(10), 
+    s_start varchar(10), -- enter as '00:00' format on 24hr clock
+    s_duration varchar(10), 
     PRIMARY KEY (s_id, s_course),
     FOREIGN KEY (s_course) REFERENCES course(c_id)
 );
@@ -84,39 +106,22 @@ GRANT SELECT, INSERT, UPDATE, DELETE, FILE ON *.* TO 'course_db_user'@'localhost
 
 -- insert into will be written below, will start with examples
 
-insert into student (std_name, std_id, std_major, std_yearofstudy, std_email) VALUES 
-    ('Tye States', 400300326, 'Computer Science', 3, '0300326s@acadiau.ca');
+insert into user(user_name, user_pass, user_role) VALUES
+    ('superadmin', '*****', 'admin');
 
-insert into department (d_name,d_code,d_email,d_phone,d_address,d_school) VALUES
+insert into department (d_name, d_code, d_email, d_phone, d_address, d_school) VALUES
     ('Jodrey School Of Computer Science', 'COMP', 'cs@acadiau.ca', 9025851331, '13 University ave. Wolfville', 'Acadia University'),
-    ('Department of Mathematics and Statistics', 'MATH', 'mathstats@acadiau.ca', 9025851382, '12 University Ave. Wolfville', 'Acadia University');
-
-insert into department (d_name,d_code,d_email,d_phone,d_address,d_school) VALUES
+    ('Department of Mathematics and Statistics', 'MATH', 'mathstats@acadiau.ca', 9025851382, '12 University Ave. Wolfville', 'Acadia University'),
     ('Manning School Of Business', 'BUSI', NULL, 9025851356, '24 University ave. Wolfville', 'Acadia University');
 
 insert into course (c_name, c_id, c_department, c_sharedwith, c_connectedlab) VALUES
-    ('Database Management Systems', 'COMP-3753', 'Jodrey School Of Computer Science', '[FA-01]', NULL),
-    ('Data Structures and Algorithms', 'COMP-2113', 'Jodrey School Of Computer Science', NULL, NULL),
-    ('Discrete Mathematics', 'MATH-1413', 'Department of Mathematics and Statistics', NULL, NULL),
-    ('Matrix Algebra', 'MATH-1323', 'Department of Mathematics and Statistics', NULL, NULL),
-    ('Translators', 'COMP-3703', 'Jodrey School Of Computer Science', NULL, NULL);
-
-insert into professor(p_name,p_email,p_department,p_office) VALUES
-    ('Dr. Elhadi Shakshuki', 'elhadi.shakshuki@acadiau.ca', '[Jodrey School Of Computer Science]', 'Carnegie Hall 313');
-
-insert into courseSection(s_id,s_course,s_loc,s_professor,s_days,s_start,s_end) VALUES
-    ('FA-01', 'COMP-3753', 'BAC 142', 'Dr. Elhadi Shakshuki', '["t","th"]', '9:30', '10:50');
+    ('Database Management Systems', 'COMP-3753', 1, '[FA-01]', NULL),
+    ('Data Structures and Algorithms', 'COMP-2113', 1, NULL, NULL),
+    ('Discrete Mathematics', 'MATH-1413', 2, NULL, NULL),
+    ('Matrix Algebra', 'MATH-1323', 2, NULL, NULL),
+    ('Translators', 'COMP-3703', 1, NULL, NULL);
 
 insert into prerequisites(c_id, pr_id) VALUES
     ('COMP-3753', 'COMP-2113'),
     ('COMP-3753', 'MATH-1413'),
     ('COMP-3753', 'MATH-1323');
-
-insert into coursesTaken(std_id, c_id) VALUES
-    (400300326, 'MATH-1413'),
-    (400300326, 'MATH-1323'),
-    (400300326, 'COMP-2113');
-
-insert into enrolled(std_id, c_id) VALUES
-    (400300326, 'COMP-3703'),
-    (400300326, 'COMP-3753');
