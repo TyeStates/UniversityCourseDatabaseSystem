ef87ccf7b464a75a01893f927c05771cb1b7d8bc
TyeStates <155793960+TyeStates@users.noreply.github.com>
2025-03-31 22:10:30 -0300

    fix endsemester.php

--- endSemester.php
+++ endSemester.php
@@ -1,7 +1,9 @@
 <?php
     session_start();
     require "connect.php";
-    $sql = "INSERT INTO coursesTaken
-            SELECT * FROM enrolled;";
-    header("home.php");
+    $db = mysqli_connect("localhost", "course_db_user", "1234", "ucprdb");
+    mysqli_query($db, "INSERT INTO coursesTaken
+                SELECT * FROM enrolled;");
+    mysqli_query($db, "TRUNCATE TABLE enrolled;");
+    header("location: home.php");
 ?>
